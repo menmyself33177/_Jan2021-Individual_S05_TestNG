37a51e50318c48ea02fa6fc42af8282d625ad525
menmyself33177 <luth_for@yahoo.com>
2021-10-20 00:09:54 -0500

    4th Commit

--- src/main/java/variousConcept/GitTest1.java
+++ src/main/java/variousConcept/GitTest1.java
@@ -6,6 +6,21 @@ public class GitTest1 {
 		
 		printMe1();
 		printMe2();
+		printMe3();
+		printMe4();
+		printMe5();
+	}
+		private static void printMe5() {
+			System.out.println("git test5");
+		
+	}
+		private static void printMe4() {
+			System.out.println("git test4");
+		
+	}
+		private static void printMe3() {
+			System.out.println("git test3");
+		
 	}
 		private static void printMe2() {
 		System.out.println("git test2");
