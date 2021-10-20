b5ecc4272a0a05f10f6d505b466583ea107fb9cb
menmyself33177 <luth_for@yahoo.com>
2021-10-20 00:00:44 -0500

    3rd Git Testing

---
+++ src/main/java/variousConcept/GitTest1.java
@@ -0,0 +1,18 @@
+package variousConcept;
+
+public class GitTest1 {
+
+	public static void main(String[] args) {
+		
+		printMe1();
+		printMe2();
+	}
+		private static void printMe2() {
+		System.out.println("git test2");
+		
+	}
+		public static void printMe1() {
+			System.out.println("git test1");
+	}
+
+}
